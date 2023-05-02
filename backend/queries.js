7d4c28f56c7d1ef6ff4410cac735fafca1f8ad89
fullvomitforme <tazkiyadigitalarchive@gmail.com>
2023-05-02 23:35:51 +0700

    rapihin

--- backend/queries.js
+++ backend/queries.js
@@ -23,7 +23,7 @@ const getUserById = (req, res) => {
 
   pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
     if (error) {
-      throw error;
+      res.status(400).json(results.error);
     }
     res.status(200).json(results.rows);
   });
